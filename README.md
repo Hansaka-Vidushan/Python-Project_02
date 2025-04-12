8425c6ee1d8dcfc32ce1ccce174ce256ae5e77ac
Hansaka-Vidushan <hansakavidushan56@gmail.com>
2025-04-12 19:21:32 +0530

    Explain Variables & Operators

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Python-Project_02
 This is my 02 python project
+Day 05
---
+++ main.py
@@ -0,0 +1,14 @@
+# Variables
+
+a = 10
+b = 3
+
+
+# Operators
+print(a + b) # 	Addition
+print(a - b) # Subtraction
+print(a * b) # 	Multiplication
+print(a / b) # 	Division
+print(a % b) # Modulus (බෙදලා ඉතිරිය)
+print(a // b) # Floor division (බෙදලා එන පූර්ණ සංඛ්‍යාව)
+print(a ** b) # Exponentiation (බලය)
